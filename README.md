404373e60a7145bf379f68f803b6a1d010a17abe
Misi <shemsimisi@gmail.com>
2023-04-07 09:32:31 +0200

    Dark mode implemented and general overhaul

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# rock-paper-scissors
+#Rock-Paper-Scissors Web Game
+
+A simple web-based game of Rock-Paper-Scissors. Play against the computer and see who wins! Built using HTML, CSS, and JavaScript. More cool features coming soon!
---
+++ script.js
@@ -0,0 +1,65 @@
+// Global variables
+const options = document.querySelectorAll('.option');
+const playerScore = document.getElementById('player-score');
+const computerScore = document.getElementById('computer-score');
+const computerChoice = document.getElementById('computer-choice');
+const resetButton = document.getElementById('reset-btn');
+let playerScoreCount = 0;
+let computerScoreCount = 0;
+
+// Function to generate computer's choice
+function computerPlay() {
+  const computerOptions = ['rock', 'paper', 'scissors'];
+  const computerChoice = computerOptions[Math.floor(Math.random() * 3)];
+  return computerChoice;
+}
+
+// Function to update score and display winner
+function updateScore(playerChoice, computerChoice) {
+  if (playerChoice === computerChoice) {
+    return 'Draw';
+  } else if (
+    (playerChoice === 'rock' && computerChoice === 'scissors') ||
+    (playerChoice === 'paper' && computerChoice === 'rock') ||
+    (playerChoice === 'scissors' && computerChoice === 'paper')
+  ) {
+    playerScoreCount++;
+    playerScore.textContent = playerScoreCount;
+    if (playerScoreCount === 5) {
+      alert('You win the game!');
+      resetGame();
+    }
+    return 'You win!';
+  } else {
+    computerScoreCount++;
+    computerScore.textContent = computerScoreCount;
+    if (computerScoreCount === 5) {
+      alert('Computer wins the game!');
+      resetGame();
+    }
+    return 'Computer wins!';
+  }
+}
+
+// Function to reset game
+function resetGame() {
+  playerScoreCount = 0;
+  computerScoreCount = 0;
+  playerScore.textContent = playerScoreCount;
+  computerScore.textContent = computerScoreCount;
+  computerChoice.src = 'rock.png';
+  computerChoice.style.opacity = 0.1;
+}
+
+// Event listeners for player's choice
+options.forEach((option) => {
+  option.addEventListener('click', () => {
+    const playerChoice = option.id;
+    const computerChoiceValue = computerPlay();
+    const computerChoiceImg = document.querySelector(`[alt="${computerChoiceValue}"]`);
+    computerChoice.src = computerChoiceImg.src;
+    computerChoice.style.opacity = 1;
+    const result = updateScore(playerChoice, computerChoiceValue);
+    console.log(result);
+  });
+});
